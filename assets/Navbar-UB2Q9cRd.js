3a36f64fc84ffb53119614dfeeb537b89c09e9c0
AbhishekNS2004 <abhishekns142@gmail.com>
2026-03-24 12:46:54 +0530

    Updates

--- assets/Navbar-UB2Q9cRd.js
+++ assets/Navbar-UB2Q9cRd.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-CQYTyzdL.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-C1j_JzB5.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
