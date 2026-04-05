71582caad4885abb6d86738999e69c97be7c3db8
AbhishekNS2004 <abhishekns142@gmail.com>
2026-04-05 15:08:39 +0530

    mm

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="#" className="navbar-title" data-cursor="disable">
-          RC
+          Abhi
         </a>
         <a
           href="mailto:abhishekns142@gmail.com"
